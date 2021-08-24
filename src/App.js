176977c8a00e8173d72e754fe7756dcd43a4af0b
NguyenPhiKhang <17520616@gm.uit.edu.vn>
2021-08-24 23:44:47 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { useEffect, useState } from 'react';
+// import swal from 'sweetalert';
+import swal from '@sweetalert/with-react'
 import './App.css';
 
 function App() {
 
   const [arrWord, setArrayWord] = useState([]);
   const [arrShow, setArrShow] = useState([]);
+  const [arrWordText, setArrWordText] = useState([]);
+  const [arrTranslate, setArrTranslate] = useState([]);
+  const [isTextTranslate, setIsTextTranslate] = useState(false);
 
   useEffect(() => {
-    if (arrShow.length === 0 && arrWord.length !== 0){
+    if (arrShow.length === 0 && arrWord.length !== 0) {
       calRandomWord();
       console.log("call");
     }
@@ -17,8 +22,8 @@ function App() {
 
   }, [arrWord, arrShow]);
 
-  function myFunction() {
-    var x = document.getElementById("myTextarea").value;
+  function loadWord() {
+    let x = document.getElementById("textWord").value;
     // arrWord = x.split('\n'); // lines is an array of strings
     document.getElementById("btnNext").hidden = false;
     document.getElementById("btnReset").hidden = true;
@@ -29,7 +34,7 @@ function App() {
     //   y = y + "'" + lines[j] + "', ";
     // }
 
-    setArrayWord(x.split("\n"));
+    setArrayWord(x.split("\n").map((v, i) => ({ idx: i, value: v })));
     setArrShow([]);
   }
 
@@ -63,7 +68,7 @@ function App() {
       idxs.push(idx);
 
       // if (arrWord.indexOf(idx) === -1) {
-        arrayShow.push(arrWord[idx]);
+      arrayShow.push(arrWord[idx]);
       // }
     }
 
@@ -87,8 +92,28 @@ function App() {
     document.getElementById("btnHide").innerHTML = document.getElementById("btnHide").textContent === "Hide" ?
       "Show" : "Hide";
 
-    document.getElementById("myTextarea").hidden = !document.getElementById("myTextarea").hidden;
+    document.getElementById("textWord").hidden = !document.getElementById("textWord").hidden;
     document.getElementById("btnLoad").hidden = !document.getElementById("btnLoad").hidden;
+  }
+
+  const showAlertTranslate = idx => {
+    if (arrWordText.length === arrTranslate.length) {
+      // swal("TRANSLATE!!!", `${arrTranslate[idx]}`);
+      swal(
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+          <span>TRANSLATE!!!</span>
+          <span style={{fontSize: '40px'}}>
+            {arrTranslate[idx]}
+          </span>
+        </div>
+      );
+    } else {
+      swal({
+        text: "Incorrect translation data!",
+        icon: "error",
+        button: "OK",
+      });
+    }
   }
 
 
@@ -99,12 +124,20 @@ function App() {
     }}>
 
       <h3>Random Word</h3>
-      {/* <br> */}
-      <div style={{ display: 'flex', alignItems: 'flex-end', marginBottom: '10px' }}>
-        <textarea id="myTextarea" style={{ minHeight: '80px', minWidth: '300px' }}></textarea>
-        <div style={{ display: 'flex', flexDirection: 'column', marginLeft: "10px" }}>
-          <button style={{ height: '20px' }} onClick={hideTextArea} id="btnHide">Hide</button>
-          <button style={{ marginTop: '10px', height: "20px" }} onClick={myFunction} id="btnLoad">Load</button>
+
+      <div style={{ marginBottom: '10px' }}>
+        <span id="spanWord">Word</span>
+        <div style={{ display: 'flex', alignItems: 'flex-end', marginBottom: '5px' }}>
+          <textarea onChange={(e) => { setArrWordText(e.target.value.split('\n')) }} id="textWord" style={{ minHeight: '80px', minWidth: '300px' }}></textarea>
+          <div style={{ display: 'flex', flexDirection: 'column', marginLeft: "10px" }}>
+            <button style={{ height: '20px', marginTop: '5px' }} onClick={hideTextArea} id="btnHide">Hide</button>
+            <button style={{ marginTop: '10px', height: "20px" }} onClick={loadWord} id="btnLoad">Load</button>
+            <button style={{ marginTop: '10px', height: "20px", marginBottom: '5px' }} onClick={() => { setIsTextTranslate(prev => !prev) }} id="btnLoad">{isTextTranslate ? "x Translate" : "+ Translate"}</button>
+          </div>
+        </div>
+        <div style={{ display: isTextTranslate ? 'flex' : 'none', flexDirection: 'column' }}>
+          <span id="spanTranslate">Translate</span>
+          <textarea onChange={(e) => { setArrTranslate(e.target.value.split('\n')) }} id="textTranslate" style={{ minHeight: '80px', minWidth: '300px', width: '300px', marginTop: '5px' }}></textarea>
         </div>
       </div>
 
@@ -125,14 +158,20 @@ function App() {
 
       {/* <br> */}
       {/* <p id="demo" style={{ fontSize: '50px' }}>{arrShow}</p> */}
-      <ul style={{ fontSize: '30px', margin: 0 }}>
+      <div style={{ marginTop: '10px' }}>
         {
-          arrShow.map((v, i)=><li key={i}>{v}</li>)
+          arrShow.map(v => (
+            <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+              <span>😄⟿</span>
+              <span style={{ fontSize: '30px', marginLeft: '6px', marginRight: '4px' }}>{v.value}</span>
+              <button onClick={() => { showAlertTranslate(v.idx) }} style={{ backgroundColor: 'rgb(52, 222, 0)', height: '18px', width: '18px', borderRadius: 9, content: 'center', padding: 0 }}>+</button>
+            </div>
+          ))
         }
-      </ul>
+      </div>
       <p id="remain">Remain: {arrWord.length}</p>
 
-      <button style={{ width: '50px' }} type="button" onClick={myFunction} id="btnReset" hidden>Reset</button>
+      <button style={{ width: '50px' }} type="button" onClick={loadWord} id="btnReset" hidden>Reset</button>
       <button style={{ width: '50px' }} type="button" onClick={btnNextAction} id="btnNext">Next</button>
 
     </div>
